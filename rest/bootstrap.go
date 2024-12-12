1ae4d81f74e01503e6fb9bf02dc0b08f8346efb7
Muhammad Bilal <148999074+mbilalsurathia@users.noreply.github.com>
2024-12-13 00:40:52 +0400

    Update bootstrap.go

--- rest/bootstrap.go
+++ rest/bootstrap.go
@@ -1,22 +1,54 @@
 package rest
 
 import (
+	"errors"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"maker-checker/service"
+	"net/http"
+	"time"
 )
 
-// StartServer initiate server
-func StartServer(container *service.Container) *HttpServer {
-
-	//Inject services instance from ServiceContainer
+func StartServer(container *service.Container) *http.Server {
+	/*
+	 */
+	gin.SetMode(gin.DebugMode)
 	userController := NewUserController(container.UserService)
+	r := gin.Default()
 
-	httpServer := NewHttpServer(
-		container.GbeConfigService.GetConfig().Rest.Addr,
-		userController,
-	)
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+	r.ForwardedByClientIP = true
 
-	go httpServer.Start()
+	api := r.Group("/api/v1")
+	{
+		user := api.Group("/message-request")
+		{
+			//paths
+			user.POST("/", userController.CreateMessage)
+			user.PATCH("/", userController.UpdateMessage)
+			user.GET("/", userController.GetMessages)
+		}
 
-	container.LoggerService.GetInstance().Info("rest server ok")
-	return httpServer
+	}
+
+	// Setup routes and middleware...
+	srv := &http.Server{
+		Addr:    container.GbeConfigService.GetConfig().Rest.Addr,
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			panic(err)
+		}
+	}()
+
+	return srv
 }
